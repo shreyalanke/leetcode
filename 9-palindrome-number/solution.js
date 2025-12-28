4c0a2269a06d288bbe23d9a1bc79886e7e25092a
shreyalanke <lankeshreya@gmail.com>
2025-12-28 20:10:24 +0530

    Day-1

---
+++ 9-palindrome-number/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    
+};
+
+console.log(isPalindrome(201));
+console.log(isPalindrome(121));
+console.log(isPalindrome(-111));
