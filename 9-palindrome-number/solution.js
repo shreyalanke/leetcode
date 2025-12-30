0c987e3ed8b1e0c78f8179e7f0fcb5f7f8292112
shreyalanke <lankeshreya@gmail.com>
2025-12-30 11:46:59 +0530

    Day-1

--- 9-palindrome-number/solution.js
+++ 9-palindrome-number/solution.js
@@ -2,8 +2,16 @@
  * @param {number} x
  * @return {boolean}
  */
+
 var isPalindrome = function(x) {
-    
+    let rev = 0;
+    let y = x
+    while(y>0){
+        rev = rev*10;
+        rev = rev+y%10;
+        y = Math.floor(y/10)
+    }
+    return x == rev;
 };
 
 console.log(isPalindrome(201));
